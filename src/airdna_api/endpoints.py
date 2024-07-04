8a626d29318868349c69a35fa7631f6218775196
Adriaan Joubert <45142747+adriaanjoubert@users.noreply.github.com>
2024-07-04 15:32:57 +0100

    Pass the bearer token from the endpoint to the client

--- src/airdna_api/endpoints.py
+++ src/airdna_api/endpoints.py
@@ -1,10 +1,24 @@
+from typing import Any
+
 from generic_api.endpoints import PostEndpoint
 
 from client import AirDNAClient
 from serializers import ReportRequestSerializer, ReportResponseSerializer
 
 
-class RentalizerEndpoint(PostEndpoint):
+class AirDNAEndpoint(PostEndpoint):
+
+    def __init__(self, *args: Any, bearer_token: str, **kwargs: Any) -> None:
+        self.bearer_token = bearer_token
+        super().__init__(*args, **kwargs)
+
+    def get_client_context(self, *args: Any, **kwargs: Any) -> dict[str, Any]:
+        client_context = super().get_client_context(*args, **kwargs)
+        client_context["bearer_token"] = self.bearer_token
+        return client_context
+
+
+class RentalizerEndpoint(AirDNAEndpoint):
     endpoint_url = "rentalizer/estimate"
     client_class = AirDNAClient
     response_entity_class = ReportResponseSerializer
